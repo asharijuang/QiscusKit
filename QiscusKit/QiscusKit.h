7e75cd3b5f3f8c59509c299cce8bd58c86423b44
asharijuang <asharijuang@me.com>
2017-09-08 10:34:27 +0700

    initial project

---
+++ QiscusKit/QiscusKit.h
@@ -0,0 +1,19 @@
+//
+//  QiscusKit.h
+//  QiscusKit
+//
+//  Created by asharijuang on 9/8/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiscusKit.
+FOUNDATION_EXPORT double QiscusKitVersionNumber;
+
+//! Project version string for QiscusKit.
+FOUNDATION_EXPORT const unsigned char QiscusKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiscusKit/PublicHeader.h>
+
+
